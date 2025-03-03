a195f66d6d7812f71ba3947580c478a9787d7460
Rushikesh Harne <rushikeshharne3097@gmail.com>
2025-03-03 09:20:25 +0530

    fa

--- new.py
+++ new.py
@@ -1,13 +1,4 @@
-<<<<<<< HEAD:app.py
 a = 5
 b = 6 
 print(a+b)
-=======
-print("welcome back")
-print("welcome back")
-print("welcome back to pull")
 
-print("welcome back")
-print("welcome back")
-print("welcome back to pull")
->>>>>>> b406c43f5879e4580774f14bf60b7ce7874d1341:new.py
